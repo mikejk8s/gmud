807cb79615c6c2f72f45e516426c3942414655c3
Mike Johnson <mike@mikej.dev>
2022-08-20 11:26:42 -0600

    Add selection menus
    Took 39 minutes

--- main.go
+++ main.go
@@ -10,9 +10,9 @@ import (
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/wish"
 	//"github.com/charmbracelet/wish/bubbletea"
 	bm "github.com/charmbracelet/wish/bubbletea"
-	"github.com/charmbracelet/wish"
 	lm "github.com/charmbracelet/wish/logging"
 	"github.com/gliderlabs/ssh"
 	"github.com/muesli/termenv"
@@ -35,7 +35,7 @@ func main() {
 			func(h ssh.Handler) ssh.Handler {
 				return func(s ssh.Session) {
 					mrj, _, _, _, _ := ssh.ParseAuthorizedKey(
-						[]byte("ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEY+lDwyfQ6DtiX7yULZiQqPLj3j763QgNwDixZwbGRD mrj@zuul.local"),
+						[]byte("ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMrr9hgSKnoddIDmzFyMnf5qb3QTsG40/9UyhexKiw6z mike@mikej.dev"),
 					)
 					switch {
 					case ssh.KeysEqual(s.PublicKey(), mrj):
@@ -44,6 +44,7 @@ func main() {
 						wish.Println(s, "User not found!")
 					}
 					h(s)
+					loginBubbleteaMiddleware()
 				}
 			},
 		),
@@ -131,7 +132,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	s := "Welcome to gmud, your terminal is: %s\n\n"
+	s := "Welcome to gmud!\n"
 	s += "Your terminal is: %s\n"
 	s += "Your window size is x: %d y: %d\n\n"
 	s += "The date is " + m.time.Format(time.RFC1123) + "\n\n"
